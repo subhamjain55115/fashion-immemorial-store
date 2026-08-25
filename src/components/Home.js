115d22c4816472c83f191d0ca8aced8c9d2fcee6
Subham Jain <subhamjain55115@users.noreply.github.com>
2026-08-25 20:37:07 +0000

    Initial commit: import fashion-immemorial-store template

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import Featured from "./Featured";
+import About from "./About";
+import Gallery from "./Gallery";
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Featured />
+      <About />
+      <Gallery />
+    </div>
+  );
+};
+
+export default Home;
